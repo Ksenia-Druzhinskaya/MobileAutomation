18714c8a1a6528ec1ed18feff020f758c6ccee42
Ksenia <Ksenia.Druzhinskaya@firstlinesoftware.com>
2022-08-05 20:10:33 +0300

    Task 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,10 +2,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
+
     @Test
     public void testGetLocalNumber(){
         int expectedLocalNumber = 14;
         MainClass mainClass = new MainClass();
         Assert.assertEquals("Local number is incorrect.", mainClass.getLocalNumber(), expectedLocalNumber);
+    }
+
+    @Test
+    public void testGetClassNumber(){
+        MainClass mainClassObject = new MainClass();
+
+        Integer comparedNumber = 45;
+        Integer actualNumber = mainClassObject.getClassNumber();
+
+        Assert.assertEquals("Class number " + actualNumber + " is not greater than " + comparedNumber,
+                            actualNumber.compareTo(comparedNumber), 1);
     }
 }
